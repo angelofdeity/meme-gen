6a9ea96d57d9cf43ce60b9c716ce0a7f1580fd8d
Nico <lawani.gk@gmail.com>
2023-07-11 19:53:45 +0100

    implement form styling with grid

--- src/components/meme.tsx
+++ src/components/meme.tsx
@@ -1,13 +1,10 @@
 export default function Meme() {
     return (
         <main>
-            <form>
-                <div className="input-section">
-                    <input className="input-box" placeholder="Shut up"></input>
-                    <input className="input-box" placeholder="and take my money"></input>
-                </div>
-                <input className="submit-meme" type="submit" value={'Get a new meme image  🖼'}></input>
-                <div className='memeImg'></div>
+            <form className="form">
+                <input className="form--input" placeholder="Top text"></input>
+                <input className="form--input" placeholder="and take my money"></input>
+                <button className="form--button">Get a new meme image  🖼</button>
             </form>
         </main>
     )
